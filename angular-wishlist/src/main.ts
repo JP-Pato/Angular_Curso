20b7c77d2bbaea259ad42a367f60f4120125835f
Juan Pablo <patostrange@vivaldi.net>
2020-10-30 07:54:54 -0300

    Pruebas automatizadas de código y actualizacion MapBox

--- angular-wishlist/src/main.ts
+++ angular-wishlist/src/main.ts
@@ -4,9 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import './polyfills';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+.bootstrapModule(AppModule)
+.then(ref) => {
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
+})
   .catch(err => console.error(err));
